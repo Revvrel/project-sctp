bf1d2c732bb8fcec6b2cd6a2b55d8c787d022bbb
Revvrel <khairul.m@hotmail.com>
2023-09-30 12:54:04 +0800

    test qr code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import React from "react";
+import QRCode from "react-qr-code";
 
 function App() {
+
   return (
     <div className="App">
+      <QRCode value = "test" />
     </div>
   );
 }
